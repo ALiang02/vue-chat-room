b367bc067ea2e6c65c3b9c7e0e0266da6e28637a
ALiang02 <1745346447@qq.com>
2020-07-23 21:33:49 +0800

    环境搭建

--- chat-room-b/src/utils/request.js
+++ chat-room-b/src/utils/request.js
@@ -8,9 +8,7 @@ const service = axios.create({
         let transData = new URLSearchParams(); //django接收的数据应该为URLSearchParams对象
         transData.append("data",JSON.stringify(data));  //将data对象转换成JSON字符串传输
         return transData;
-    },
-  
-    
+    }
 })
 
 export default service
